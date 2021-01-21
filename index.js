7f402920fe21f5ab3200b6b2dc78401ca54d512e
rbolet <rapha.bolet@gmail.com>
2021-01-21 14:01:15 -0800

    command switch statement working as intended

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const arguments = require("minimist")(process.argv.slice(2));
+const minimist = require("minimist");
+
 module.exports = () => {
   console.log("DON'T PANIC");
-  console.log(arguments);
-  console.log(Array.isArray(arguments._));
+  const arguments = minimist(process.argv.slice(2));
+  const command = arguments?._[0];
+
+  switch (command) {
+    case "towel":
+      console.log("You're one hoopy frood!");
+      break;
+    default:
+      if (command === undefined) {
+        console.log("Enter a valid command.");
+      } else {
+        console.log(`${command} is not a valid command.`);
+      }
+  }
 };
